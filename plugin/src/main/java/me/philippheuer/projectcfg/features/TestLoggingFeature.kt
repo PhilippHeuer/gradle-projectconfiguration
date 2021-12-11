d52be3d3a2ec358d8a587f5baa9bc5b908093c0c
Philipp Heuer <git@philippheuer.me>
2021-12-11 17:38:52 +0100

    feat: don't apply test logging plugin when running from intellij

--- plugin/src/main/java/me/philippheuer/projectcfg/features/TestLoggingFeature.kt
+++ plugin/src/main/java/me/philippheuer/projectcfg/features/TestLoggingFeature.kt
@@ -7,14 +7,23 @@ import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
 
 class TestLoggingFeature constructor(override var project: Project, override var config: ProjectConfigurationExtension) : PluginModule {
+    private val isIDEA = System.getProperty("idea.fatal.error.notification") != null
+
     override fun check(): Boolean {
+        // do not apply plugins when running in intellij
+        if (isIDEA) {
+            return false
+        }
+
         return true
     }
 
     override fun run() {
+        // plugin
         log(LogLevel.INFO, "applying plugin [com.adarshr.test-logger]")
         project.pluginManager.apply("com.adarshr.test-logger")
 
+        // configure
         project.extensions.configure(TestLoggerExtension::class.java) {
             it.setTheme("mocha-parallel")
             it.slowThreshold = 1000
