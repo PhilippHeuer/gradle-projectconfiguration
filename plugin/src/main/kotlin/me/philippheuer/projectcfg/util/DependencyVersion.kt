f47d7089a737227b6f291d3e666feb4b7491457d
Philipp Heuer <git@philippheuer.me>
2022-07-08 00:26:08 +0200

    feat: update dependencies and add renovate comments for updates

--- plugin/src/main/kotlin/me/philippheuer/projectcfg/util/DependencyVersion.kt
+++ plugin/src/main/kotlin/me/philippheuer/projectcfg/util/DependencyVersion.kt
@@ -2,20 +2,34 @@ package me.philippheuer.projectcfg.util
 
 open class DependencyVersion {
     companion object {
-        @JvmStatic var kotlinVersion: String = "1.6.21" // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-        @JvmStatic var junit5Version: String = "5.8.2" // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-api
-        @JvmStatic var mockitoVersion: String = "4.5.1" // https://mvnrepository.com/artifact/org.mockito/mockito-core
-        @JvmStatic var mockitoKotlinVersion: String = "4.0.0" // https://mvnrepository.com/artifact/org.mockito.kotlin/mockito-kotlin
-        @JvmStatic var springBootVersion: String = "2.6.7" // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter
-        @JvmStatic var springNativeVersion: String = "0.11.2" // https://mvnrepository.com/artifact/org.springframework.experimental/spring-native?repo=springio-libs-release - springNativeVersion needs to be compatible with springBootVersion, see https://docs.spring.io/spring-native/docs/current/reference/htmlsingle/#_validate_spring_boot_version
-        @JvmStatic var quarkusVersion: String = "2.9.1.Final" // https://mvnrepository.com/artifact/io.quarkus/quarkus-core
-        @JvmStatic var kotlinLoggingVersion: String = "2.1.21" // https://mvnrepository.com/artifact/io.github.microutils/kotlin-logging
-        @JvmStatic var availabilityVersion: String = "4.2.0" // https://mvnrepository.com/artifact/org.awaitility/awaitility
-        @JvmStatic var slf4jVersion: String = "1.7.36" // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-        @JvmStatic var disruptorVersion: String = "3.4.4" // https://mvnrepository.com/artifact/com.lmax/disruptor
-        @JvmStatic var sentryVersion: String = "5.7.4" // https://mvnrepository.com/artifact/io.sentry/sentry
-        @JvmStatic var log4j2Version: String = "2.17.2" // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j
-        @JvmStatic var jandexVersion: String = "2.4.2.Final" // https://mvnrepository.com/artifact/org.jboss/jandex
-        @JvmStatic var micrometerVersion: String = "1.9.0" // https://mvnrepository.com/artifact/io.micrometer/micrometer-bom
+        // renovate: datasource=maven depName=org.jetbrains.kotlin:kotlin-stdlib
+        @JvmStatic var kotlinVersion: String = "1.7.10"
+        // renovate: datasource=maven depName=org.junit.jupiter:junit-jupiter-api
+        @JvmStatic var junit5Version: String = "5.8.2"
+        // renovate: datasource=maven depName=org.mockito:mockito-core
+        @JvmStatic var mockitoVersion: String = "4.6.0"
+        // renovate: datasource=maven depName=org.mockito.kotlin:mockito-kotlin
+        @JvmStatic var mockitoKotlinVersion: String = "4.0.0"
+        // renovate: datasource=maven depName=org.springframework.boot:spring-boot-starter
+        @JvmStatic var springBootVersion: String = "2.7.1"
+        @JvmStatic var springNativeVersion: String = "0.11.2"
+        // renovate: datasource=maven depName=io.quarkus:quarkus-core
+        @JvmStatic var quarkusVersion: String = "2.10.2.Final"
+        // renovate: datasource=maven depName=io.github.microutils:kotlin-logging
+        @JvmStatic var kotlinLoggingVersion: String = "2.1.23"
+        // renovate: datasource=maven depName=org.awaitility:awaitility
+        @JvmStatic var availabilityVersion: String = "4.2.0"
+        // renovate: datasource=maven depName=org.slf4j:slf4j-api
+        @JvmStatic var slf4jVersion: String = "1.7.36"
+        // renovate: datasource=maven depName=com.lmax:disruptor
+        @JvmStatic var disruptorVersion: String = "3.4.4"
+        // renovate: datasource=maven depName=io.sentry:sentry
+        @JvmStatic var sentryVersion: String = "6.1.4"
+        // renovate: datasource=maven depName=org.apache.logging.log4j:log4j
+        @JvmStatic var log4j2Version: String = "2.18.0"
+        // renovate: datasource=maven depName=org.jboss:jandex
+        @JvmStatic var jandexVersion: String = "2.4.3.Final"
+        // renovate: datasource=maven depName=io.micrometer:micrometer-bom
+        @JvmStatic var micrometerVersion: String = "1.9.1"
     }
 }
