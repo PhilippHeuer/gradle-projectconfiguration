a6b684c9bc7f6277bfd588b21b5c81d71d7a592f
Philipp Heuer <git@philippheuer.me>
2023-05-07 00:39:44 +0200

    chore: remove unused dependencies from DependencyVersion

--- plugin/src/main/kotlin/me/philippheuer/projectcfg/util/DependencyVersion.kt
+++ plugin/src/main/kotlin/me/philippheuer/projectcfg/util/DependencyVersion.kt
@@ -6,30 +6,9 @@ open class DependencyVersion {
         @JvmStatic var kotlinVersion: String = "1.8.21"
         // renovate: datasource=maven depName=org.junit.jupiter:junit-jupiter-api
         @JvmStatic var junit5Version: String = "5.9.3"
-        // renovate: datasource=maven depName=org.mockito:mockito-core
-        @JvmStatic var mockitoVersion: String = "5.3.1"
-        // renovate: datasource=maven depName=org.mockito.kotlin:mockito-kotlin
-        @JvmStatic var mockitoKotlinVersion: String = "4.1.0"
-        // renovate: datasource=maven depName=org.springframework.boot:spring-boot-starter
-        @JvmStatic var springBootVersion: String = "3.0.6"
-        @JvmStatic var springNativeVersion: String = "0.11.2"
-        // renovate: datasource=maven depName=io.quarkus:quarkus-core
-        @JvmStatic var quarkusVersion: String = "3.0.2.Final"
         // renovate: datasource=maven depName=io.github.microutils:kotlin-logging
         @JvmStatic var kotlinLoggingVersion: String = "3.0.5"
-        // renovate: datasource=maven depName=org.awaitility:awaitility
-        @JvmStatic var availabilityVersion: String = "4.2.0"
         // renovate: datasource=maven depName=org.slf4j:slf4j-api
         @JvmStatic var slf4jVersion: String = "2.0.7"
-        // renovate: datasource=maven depName=com.lmax:disruptor
-        @JvmStatic var disruptorVersion: String = "3.4.4"
-        // renovate: datasource=maven depName=io.sentry:sentry
-        @JvmStatic var sentryVersion: String = "6.18.1"
-        // renovate: datasource=maven depName=org.apache.logging.log4j:log4j
-        @JvmStatic var log4j2Version: String = "2.20.0"
-        // renovate: datasource=maven depName=org.jboss:jandex
-        @JvmStatic var jandexVersion: String = "3.1.1"
-        // renovate: datasource=maven depName=io.micrometer:micrometer-bom
-        @JvmStatic var micrometerVersion: String = "1.10.6"
     }
 }
